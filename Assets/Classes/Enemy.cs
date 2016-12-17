97e941e337680793c4e3ffe75f91077e3b6ba948
Ateş Balcı <atesbalci@gmail.com>
2016-12-17 18:42:36 +0300

    implemented enemy and added prefab

---
+++ Assets/Classes/Enemy.cs
@@ -0,0 +1,30 @@
+﻿using UnityEngine;
+using System.Collections;
+
+public class Enemy : MonoBehaviour {
+	private const float SPEED = 5;
+
+	public Node startNode;
+
+	public float health;// { get; set; }
+
+	private Node nextNode;
+
+	void Start() {
+		nextNode = startNode;
+		health = 1;
+	}
+
+	void Update() {
+		if(health <= 0) {
+			Destroy(gameObject);
+			return;
+		}
+		if (nextNode) {
+			transform.position = Vector3.MoveTowards(transform.position,
+				nextNode.transform.position, SPEED * Time.deltaTime);
+			if (Vector3.Distance(transform.position, nextNode.transform.position) < 0.001f)
+				nextNode = nextNode.next;
+		}
+	}
+}
