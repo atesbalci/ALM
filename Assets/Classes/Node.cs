7b554c3c9dd9b1eb2183db32df8ac84292c51fe1
Ateş Balcı <atesbalci@gmail.com>
2016-12-17 18:43:48 +0300

    added materials for visualization of the level as well as enemy health bars

---
+++ Assets/Classes/Node.cs
@@ -0,0 +1,20 @@
+﻿using UnityEngine;
+using System.Collections;
+
+public class Node : MonoBehaviour {
+	public Node next;
+
+	void OnDrawGizmos() {
+		if (next) {
+			Gizmos.color = Color.white;
+			Gizmos.DrawLine(transform.position, next.transform.position);
+		}
+		Gizmos.color = Color.cyan;
+		Gizmos.DrawSphere(transform.position, 0.1f);
+	}
+
+	void OnDrawGizmosSelected() {
+		Gizmos.color = Color.yellow;
+		Gizmos.DrawSphere(transform.position, 0.1f);
+	}
+}
---
+++ Assets/Classes/UI/EnemyCanvas.cs
@@ -0,0 +1,18 @@
+﻿using UnityEngine;
+using UnityEngine.UI;
+using System.Collections;
+
+public class EnemyCanvas : MonoBehaviour {
+	public Image healthBar;
+
+	private Enemy enemy;
+
+	void Start() {
+		enemy = transform.parent.GetComponent<Enemy>();
+	}
+
+	void Update() {
+		healthBar.fillAmount = enemy.health * 0.5f;
+		healthBar.color = Color.Lerp(Color.red, Color.green, enemy.health);
+	}
+}
