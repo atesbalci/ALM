32556d11ed309ff78deacfa838f72cd09a9dd5f3
Ateş Balcı <atesbalci@gmail.com>
2016-12-29 14:09:54 +0300

    Addressed Task: https://app.asana.com/0/232505586776615/232505586776657

--- Assets/Editor/Test.cs
+++ Assets/Editor/Test.cs
@@ -7,16 +7,11 @@ public class Test {
 	[Test]
 	public void EditorTest()
 	{
-		//Arrange
 		var gameObject = new GameObject();
-
-		//Act
-		//Try to rename the GameObject
+		
 		var newGameObjectName = "My game object";
 		gameObject.name = newGameObjectName;
-
-		//Assert
-		//The object has a new name
+		
 		Assert.AreEqual(newGameObjectName, gameObject.name);
 	}
 }
